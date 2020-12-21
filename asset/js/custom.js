4beadbfcc4f2207f1a0caed303c0661f265d726e
devshisir <contact.shisirimran@gmail.com>
2020-12-22 04:23:45 +0600

    Initial Commit

---
+++ asset/js/custom.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    var school = $(window);
+    var page = $('html, body');
+
+
+    // start section 2 wheel slider
+    
+    var owl = $('.section2SliderActive');
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    margin:10,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    smartSpeed: 1000,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },            
+        960:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+//Firefox:
+owl.on('DOMMouseScroll','.owl-stage',function(e){
+    if (e.originalEvent.detail > 0){ 
+        owl.trigger('next.owl');
+        } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+    });
+
+//Chrome, IE
+owl.on('mousewheel','.owl-stage',function(e){
+    if (e.originalEvent.wheelDelta > 0){
+        owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
+owl.on('translate.owl.carousel', function (event) {
+    $('.section2SliderItem h4').removeClass('animate__animated animate__fadeInDown').css("opacity","0");
+})
+owl.on('translated.owl.carousel', function (event) {
+    $('.section2SliderItem h4').addClass('animate__animated animate__fadeInDown').css("opacity","1");
+})
+
+
+
+
+
+
+
+
+    
+});
